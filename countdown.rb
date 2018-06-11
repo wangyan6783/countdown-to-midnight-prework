5d6b63a026350cc5914e944bf11103459e6a22d5
Yan Wang <wangyan6783@gmail.com>
2018-06-11 14:49:32 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
 
-def countdown
+def countdown(num)
+  second = num
+  while second > 0
+    puts "#{second} SECOND(S)!"
+    second -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep(num)
+  second = num
+  while second > 0
+    puts "#{second} SECOND(S)!"
+    sleep 1
+    second -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
