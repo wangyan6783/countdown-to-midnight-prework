514dbde072cc53e7ad9f382a216e1b72ab70938a
Yan Wang <wangyan6783@gmail.com>
2018-06-11 14:48:46 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,13 @@ def countdown(num)
   end
   "HAPPY NEW YEAR!"
 end
+
+
+def countdown_with_sleep(num)
+  second = num
+  while second > 0
+    puts "#{second} SECOND(S)!"
+    second -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
